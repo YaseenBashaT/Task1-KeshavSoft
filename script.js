784c593b597d17d591d8835f87c380ce69127d50
Thippaluri Yaseen Basha <yasenroycey6@gmail.com>
2025-11-09 20:41:21 +0530

    Add scroll effect for navbar background change

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar scroll effect
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.style.background = 'rgba(0,0,0,0.95)';
+  } else {
+    navbar.style.background = 'rgba(0,0,0,0.8)';
+  }
+});
